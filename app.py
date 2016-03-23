dd8722ea82fe75dd2ea168be64a4f737e3b4d3bc
Mike Ess <msylvest55@gmail.com>
2016-03-23 08:32:23 -0500

    this is a commit

--- app.py
+++ app.py
@@ -18,27 +18,28 @@ def hello_monkey():
 
     from_number = request.values.get('From', None)
 
-    if from_number in callers:
-        message = callers[from_number] + ", thanks for the message!"
-    else:
-        message = "Monkey, thanks for the message!"
+    # if from_number in callers:
+    #     message = callers[from_number] + ", thanks for the message!"
+    # else:
+    #     message = "Monkey, thanks for the message!"
 
 
-    ####
-    if from_number is None:
-        user_channel = '#noNumber'
-    else:
-        user_channel = '#' + from_number
+    # ####
+    # if from_number is None:
+    #     user_channel = '#noNumber'
+    # else:
+    #     user_channel = '#' + from_number
 
-    slack.channels.join(user_channel)
+    # slack.channels.join(user_channel)
 
-    #slack.chat.post_message('#general', 'Hello fellow slackers!')
-    slack.chat.post_message(user_channel, 'Hello fellow slackers!')
+    # slack.chat.post_message('#general', 'Hello fellow slackers!')
+    slack.chat.post_message('#general', from_number)
+    #slack.chat.post_message(user_channel, 'Hello fellow slackers!')
 
     ###
 
     resp = twilio.twiml.Response()
-    resp.message(message)
+   # resp.message(message)
 
     return str(resp)
 
