326480bd01806e02061ddb32923d72527213f6d6
Mike Ess <msylvest55@gmail.com>
2016-03-26 20:19:29 -0500

    this is a test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'a secret key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 slack = Slacker('xoxp-28038241029-28038195079-28266163220-166a67df32')
-
+print(slack.channels.get_channel_id('new'))
 
 @app.route('/')
 def index():
@@ -33,6 +33,7 @@ def bullshit():
         print(channelName)
         #slacker.channels.info(channelName)
         print(slack.channels.get_channel_id('general'))
+        print(slack.channels.get_channel_id('new'))
         slack.channels.archive("new")
         return request.form.get("text") + channelName 
      return 'Hello World'
