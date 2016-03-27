a2118b9ca16779aeda667fd1320b1dec1541b16c
Mike Ess <msylvest55@gmail.com>
2016-03-27 01:27:03 -0500

    fucker

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def bullshit():
         channelName = request.form.get("channel_name")
         print(slack.channels.get_channel_id('new'))
         unArch = slack.channels.get_channel_id('new')
-        slack.channels.archive(unArch)
+        slack.channels.unarchive(unArch)
         return request.form.get("text") + channelName 
      return 'Hello World'
 
