83d83d56db8483aa068d67fca4eeed92cab15693
Mike Ess <msylvest55@gmail.com>
2016-03-27 01:23:26 -0500

    hello

--- app.py
+++ app.py
@@ -8,11 +8,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 slack = Slacker('xoxp-28038241029-28038195079-28266163220-166a67df32')
 print(slack.channels.get_channel_id('new'))
-#slack.channels.create("new3")
-#slack.channels.unarchive("new2")
+
 @app.route('/')
-
-
 def index():
     return 'Index Page'
 
@@ -25,17 +22,7 @@ def bullshit():
 
      if request.method == 'POST':
         channelName = request.form.get("channel_name")
-        # 
-        
-        # print(channelName)
-        # #slacker.channels.info(channelName)
-        # print(slack.channels.get_channel_id('general'))
-        # print(slack.channels.get_channel_id('new'))
-        # genID = slack.channels.get_channel_id('general')
-        # newID = slack.channels.get_channel_id('new')
-        newID = slack.channels.get_channel_id('new3')
-        print(newID)
-        slack.channels.unarchive(newID)
+     
         return request.form.get("text") + channelName 
      return 'Hello World'
 
